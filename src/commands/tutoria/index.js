7335bad1688ea63dcf4fce145ff72deb1700ef54
KingTimer12 <queentimer12@gmail.com>
2024-04-08 15:14:28 -0300

    feat: added tutor command;

---
+++ src/commands/tutoria/index.js
@@ -0,0 +1,97 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require(`discord.js`)
+const { getTutor } = require(`./tutorApi`)
+const { addStudent, updateStudentTutor, existStudent } = require(`./studentApi`)
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName(`tutoria`)
+        .setDescription(`Comando da tutoria`)
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName(`adicionar`)
+                .setDescription(`Adicione um novo tutorando.`)
+                .addUserOption((option) =>
+                    option
+                        .setName(`tutorando`)
+                        .setDescription(`O user do novo tutorando. Pode usar o id.`)
+                        .setRequired(true)
+                )
+                .addUserOption((option) =>
+                    option.setName(`tutor`).setDescription(`O tutor do novo tutorando.`)
+                )
+        )
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName(`alterar`)
+                .setDescription(`Altere o tutor de um tutorando.`)
+                .addUserOption((option) =>
+                    option
+                        .setName(`tutorando`)
+                        .setDescription(`O user do tutorando. Pode usar o id.`)
+                        .setRequired(true)
+                )
+                .addUserOption((option) =>
+                    option.setName(`tutor`).setDescription(`O novo tutor do tutorando.`)
+                )
+        )
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName(`remover`)
+                .setDescription(`Remova o tutorando da tutoria.`)
+                .addUserOption((option) =>
+                    option
+                        .setName(`tutorando`)
+                        .setDescription(`O user do tutorando. Pode usar o id.`)
+                        .setRequired(true)
+                )
+        )
+        .setDefaultMemberPermissions(PermissionFlagsBits.CreatePublicThreads)
+    ,
+
+    /** 
+     * @param {import("discord.js").Client} client
+     * @param {import("discord.js").CommandInteraction} interaction
+     */
+
+    run: async (client, interaction) => {
+        const token = client.tokenApi
+        const { options } = interaction
+        const subcommand = options.data[0]
+
+        const target = options.getUser(`tutorando`)
+        const has = await existStudent(target.id, token)
+
+        if (subcommand.name == `adicionar`) {
+            const tutorUser = options.getUser(`tutor`)
+            let Tutor = `SemTutor`
+            if (tutorUser) {
+                const tutorData = await getTutor(tutorUser.id, token)
+                if (!tutorData.tutor) return await interaction.reply(`O tutor \`${tutorUser.username}\` não está registrado.`)
+                Tutor = tutorData.tutor
+            }
+            const member = await client.guilds.cache.map(async g => await g.members.fetch({ user: target })).at(0)
+            if (has) await updateStudentTutor(target.id, { tutor: Tutor }, token)
+            else await addStudent(target.id, { AtualUsername: target.username, Nickname: member.nickname, Tutor }, token)
+            return await interaction.reply(`O \`${target.username}\` foi adicionado como tutorando de **${Tutor}**.`)
+        }
+
+        if (subcommand.name == `alterar`) {
+            if (!has) return await interaction.reply(`O tutorando \`${target.username}\` não está registrado.`)
+            const tutorUser = options.getUser(`tutor`)
+            let tutor = `SemTutor`
+            if (tutorUser) {
+                const tutorData = await getTutor(tutorUser.id, token)
+                if (!tutorData.tutor) return await interaction.reply(`O tutor \`${tutorUser.username}\` não está registrado.`)
+                tutor = tutorData.tutor
+            }
+            await updateStudentTutor(target.id, { tutor }, token)
+            return await interaction.reply(`O \`${target.username}\` foi alterado para **${tutor}**.`)
+        }
+
+        if (subcommand.name == `remover`) {
+            if (!has) return await interaction.reply(`O tutorando \`${target.username}\` não está registrado.`)
+            await updateStudentTutor(target.id, { tutor: `Inativo` }, token)
+            return await interaction.reply(`O \`${target.username}\` foi removido da tutoria.`)
+        }
+    },
+}
---
+++ src/commands/tutoria/studentApi.js
@@ -0,0 +1,58 @@
+const { default: axios } = require(`axios`)
+
+/**
+ * @param {String} userId
+ * @param {{ AtualUsername: String, Nickname: String, Tutor: String }} data
+ * @param {String} token
+ * @returns {Promise<void>}
+*/
+const addStudent = async (userId, data, token) => {
+    try {
+        await axios.post(`${process.env.NEBI_API_URL}/student/${userId}`, data, {
+            headers: {
+                Authorization: token
+            }
+        })
+    } catch (err) {
+        throw Error(`Erro ao fazer conexão.`)
+    }
+}
+
+/**
+ * @param {String} userId
+ * @param {{ tutor: String }} data
+ * @param {String} token
+ * @returns {Promise<void>}
+*/
+const updateStudentTutor = async (userId, data, token) => {
+    try {
+        await axios.put(`${process.env.NEBI_API_URL}/student/${userId}/data`, data, {
+            headers: {
+                Authorization: token
+            }
+        })
+    } catch (err) {
+        throw Error(`Erro ao fazer conexão.`)
+    }
+}
+
+/**
+ * @param {String} userId
+ * @param {{ tutor: String }} data
+ * @param {String} token
+ * @returns {Promise<boolean>}
+*/
+const existStudent = async (userId, token) => {
+    try {
+        const response = await axios.get(`${process.env.NEBI_API_URL}/student/${userId}`, {
+            headers: {
+                Authorization: token
+            }
+        })
+        return response.data.result
+    } catch (err) {
+        throw Error(`Erro ao fazer conexão.`)
+    }
+}
+
+module.exports = { addStudent, updateStudentTutor, existStudent }
